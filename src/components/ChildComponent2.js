ce260a69fcd71fe32296dcaac86ba5408d760c8d
Satyam <nikamsatyam187@gmail.com>
2025-12-04 13:21:18 +0530

    Update ChildComponent2.js

--- src/components/ChildComponent2.js
+++ src/components/ChildComponent2.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const ChildComponent2 = (props) => {
-  const handleClick = (option) => {
-    props.onButtonClick(option);
-  };
-
+const ChildComponent2 = ({ setSelectedOption }) => {
   return (
-    <div id="child2">
-      <h3>Child Component 2</h3>
-      <button onClick={() => handleClick('Option A')}>Option A</button>
-      <button onClick={() => handleClick('Option B')}>Option B</button>
+    <div>
+      <button onClick={() => setSelectedOption('Option A')}>
+        Option A
+      </button>
+      <button onClick={() => setSelectedOption('Option B')}>
+        Option B
+      </button>
     </div>
   );
 };

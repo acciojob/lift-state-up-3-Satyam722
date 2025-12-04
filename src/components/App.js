337c90e30222fdd67a445dc6ae874eef9acd66ab
Satyam <nikamsatyam187@gmail.com>
2025-12-04 13:04:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-
-import React from "react";
+import React, { useState } from 'react';
+import ChildComponent1 from './ChildComponent1';
+import ChildComponent2 from './ChildComponent2';
 import './../styles/App.css';
 
-const App = () => {
+function App() {
+  // State as per requirements
+  const [selectedOption, setSelectedOption] = useState("");
+
+  // Handler to update state from child components
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="main">
+      <h1>Parent Component</h1>
+      <p>Selected Option: <strong>{selectedOption || "None"}</strong></p>
+      
+      <div className="child-container">
+        <ChildComponent1 
+          onOptionSelect={handleOptionSelect} 
+          selectedOption={selectedOption}
+        />
+        
+        <ChildComponent2 
+          onOptionSelect={handleOptionSelect} 
+          selectedOption={selectedOption}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

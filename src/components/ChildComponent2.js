52636b5e1009f18957f05ce59624f36dc7f08128
Satyam <nikamsatyam187@gmail.com>
2025-12-15 13:26:16 +0530

    Update ChildComponent2.js

--- src/components/ChildComponent2.js
+++ src/components/ChildComponent2.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const ChildComponent2 = ({ onSelect }) => {
-  return (
-     <div className="child child2">
-      <h2>Child Component 2</h2>
-      <button onClick={() => onSelect("Option 2")}>Option 2</button>
-    </div>
-  );
-};
+function ChildComponent2({onSelect}){
+    return(
+        <div style={{width:"500px",height:"100px", backgroundColor:"yellow"}}>
+         <h2>Child Component 2</h2>
+
+         <button onClick={()=>onSelect("Option 2")}>Option 2</button>
+        </div>
+    )
+}
 
 export default ChildComponent2;
